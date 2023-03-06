ee60843a3ba2ebd628f10f22e5d216e9c48bf328
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2023-03-06 21:09:30 +0300

    HW-2
    Add StackList

---
+++ HW-2/IStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StackCalculator
+{
+
+    /// Interface for stack.
+    public interface IStack
+    {
+        /// Checks for emptiness
+        /// returns true if the stack is empty
+        bool IsEmpty();
+
+
+        /// Adds element to a top of the stack
+        /// param is element to add
+        void Push(int value);
+
+
+        /// Gets element from a top of the stack and removes it
+        /// returns element that was on the top
+        int Pop();
+
+
+        /// Removes all elements from the stack
+        void Clear();
+    }
+}
+
