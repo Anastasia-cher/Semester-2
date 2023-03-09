1dc02aa525f0e962230f3b46fa8c03b859be7318
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2023-03-09 21:36:20 +0300

    Hw-2

--- HW-2/StackCalculator/Program.cs
+++ HW-2/StackCalculator/Program.cs
@@ -1,22 +1,12 @@
-﻿using System;
+﻿
+Console.WriteLine("Enter:");
+Console.WriteLine("1 - use list-based stack");
+Console.WriteLine("2 - use array-based stack");
+Console.WriteLine("To calculate postfix expression");
 
-namespace StackCalculator;
-
-internal class Program
+var input = Console.ReadLine();
+if (!int.TryParse(input, out int choice))
 {
-
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Enter:");
-        Console.WriteLine("1 - use list-based stack");
-        Console.WriteLine("2 - use array-based stack");
-        Console.WriteLine("To calculate postfix expression");
-
-        var input = Console.ReadLine();
-        if (!int.TryParse(input, out int choice))
-        {
-            Console.WriteLine("Invalid input");
-            return;
-        }
-    }
+    Console.WriteLine("Invalid input");
+    return;
 }
