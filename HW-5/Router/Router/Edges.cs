8ab134a05dc10d56d67f295ca847a555ab7b1310
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2023-04-07 22:05:45 +0300

    Edges

---
+++ HW-5/Router/Router/Edges.cs
@@ -0,0 +1,22 @@
+﻿namespace Router;
+
+/// <summary>
+/// A class representing edges in an undirected graph.
+/// </summary>
+public class Edges
+{
+    public int FromVertex { get; set; }
+    public int ToVertex { get; set; }
+
+    /// <summary>
+    /// The bandwidth between the start and end vertices.
+    /// </summary>
+    public int Bandwidth { get; set; }
+
+    internal Edges(int fromVertex, int toVertex, int bandwidth)
+    {
+        FromVertex = fromVertex;
+        ToVertex = toVertex;
+        Bandwidth = bandwidth;
+    }
+}
